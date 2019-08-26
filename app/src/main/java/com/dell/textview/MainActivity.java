6070c61f3aaeef353e6a0d1663dd35bce699a737
Abdullahfoysal <abdullah32@student.sust.edu>
2019-08-26 22:02:40 +0600

    TextView and scrolling

---
+++ app/src/main/java/com/dell/textview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dell.textview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTextView =(TextView) findViewById(R.id.nameTextViewId);
+         // nameTextView.setText("Foysal");
+        nameTextView.setTextSize(30);
+    }
+}
