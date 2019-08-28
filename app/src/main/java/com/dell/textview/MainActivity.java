8c8b69d8f7192bf3fe66fcd0aa6c1730f69ca788
Abdullahfoysal <abdullah32@student.sust.edu>
2019-08-28 15:59:59 +0600

    buttonClick

--- app/src/main/java/com/dell/textview/MainActivity.java
+++ app/src/main/java/com/dell/textview/MainActivity.java
@@ -2,19 +2,39 @@ package com.dell.textview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int count=0;
+    private Button loginButton,logoutButton;
+    private TextView textView;
 
-    private TextView nameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameTextView =(TextView) findViewById(R.id.nameTextViewId);
-         // nameTextView.setText("Foysal");
-        nameTextView.setTextSize(30);
+        loginButton =(Button) findViewById(R.id.loginButtonId);
+        textView =(TextView) findViewById(R.id.textViewId);
+        logoutButton =(Button) findViewById(R.id.logoutButtonId);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                textView.setText("Login button is clicked"+ ++count + "Times");
+                textView.setTextColor(Color.RED);
+            }
+        });
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Logout button clicked ");
+            }
+        });
     }
 }
