993a0d99500620e8afd150e2ea3e4dc06a350ab8
Abdullahfoysal <abdullah32@student.sust.edu>
2019-08-28 21:31:58 +0600

    get data from edit textbox

--- app/src/main/java/com/dell/textview/MainActivity.java
+++ app/src/main/java/com/dell/textview/MainActivity.java
@@ -9,13 +9,15 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     int count=0;
     private Button loginButton,logoutButton;
     private TextView textView;
+    private EditText editText1,editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         loginButton =(Button) findViewById(R.id.loginButtonId);
         logoutButton =(Button) findViewById(R.id.logoutButtonId);
         textView =(TextView) findViewById(R.id.textViewId);
+
+        editText1= (EditText)findViewById(R.id.editText1Id);
+        editText2= (EditText)findViewById(R.id.editText2Id);
+
+        loginButton.setOnClickListener(this);
+        logoutButton.setOnClickListener(this);
+
 
 
 
@@ -39,6 +48,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    @Override
+    public void onClick(View v) {
+        String number1=editText1.getText().toString();
+        String number2=editText2.getText().toString();
 
+        //convert to double
+        double num1,num2;
+        if(!number1.isEmpty()) {
+             num1 = Double.parseDouble(number1);
+            num2 = Double.parseDouble(number2);
+        }
+        else {num1=0.0;num2=0.0;}
 
+        if(v.getId()==R.id.loginButtonId)
+        {
+            double sum=num1+num2;
+            textView.setText("Result ="+sum);
+        }
+        if(v.getId()==R.id.logoutButtonId)
+        {
+            double sub=num1-num2;
+            textView.setText("Result ="+sub);
+        }
+
+    }
 }
