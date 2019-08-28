04fe3ca1ae64b2ace7a9de5ebcedbcacc38581ac
Abdullahfoysal <abdullah32@student.sust.edu>
2019-08-28 16:21:19 +0600

    buttonClick handler

--- app/src/main/java/com/dell/textview/MainActivity.java
+++ app/src/main/java/com/dell/textview/MainActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
     int count=0;
     private Button loginButton,logoutButton;
     private TextView textView;
@@ -22,16 +23,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView =(TextView) findViewById(R.id.textViewId);
         logoutButton =(Button) findViewById(R.id.logoutButtonId);
 
-        logoutButton.setOnClickListener(this);
-        loginButton.setOnClickListener(this);
+        Handler handler=new Handler();
+        loginButton.setOnClickListener(handler);
+        logoutButton.setOnClickListener(handler);
 
     }
+    class Handler implements View.OnClickListener{
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.loginButtonId)
-        textView.setText("Login Button");
-        if(v.getId()==R.id.logoutButtonId)
-        textView.setText("Logout Button");
+        @Override
+        public void onClick(View v) {
+            if(v.getId()==R.id.loginButtonId)
+                textView.setText("Login Button");
+            if(v.getId()==R.id.logoutButtonId)
+                textView.setText("Logout Button");
+        }
     }
+
+
+
 }
